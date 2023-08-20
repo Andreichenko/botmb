2be4fb738c3f5c4674772c126f4b1d2ba790aa8c
aleksandrandreichenko <andreichenko@web.de>
2023-08-20 23:10:01 +0300

    Added the main class for webapp

---
+++ src/main/java/org/caller/botmb/VirtualBombingApplication.java
@@ -0,0 +1,12 @@
+package org.caller.botmb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualBombingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualBombingApplication.class, args);
+    }
+
+}
